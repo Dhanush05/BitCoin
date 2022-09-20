899812881419a7c12c8514ba769fb1fce8787509
Akshith <dhanushpakanati@gmail.com>
2022-09-20 13:42:32 -0400

    Working with single actor

--- src/cypher.erl
+++ src/cypher.erl
@@ -10,14 +10,47 @@
 -author("dhanush").
 
 %% API
--export([main/0]).
-main()->
-  createHash("string example").
+-export([main/1,createActors/0]).
+main(K)->
+  stringGenerator(12,"qwertyQWERTY1234567890").
+
+stringGenerator(Length, AllowedChars)->
+  _Gs = lists:foldl(fun(_,Acc)->
+    [lists:nth(random:uniform(length(AllowedChars)),AllowedChars)]
+      ++Acc
+    end, [], lists:seq(1,Length)),
+  _prefix = "dpakanati;",
+  _Test = "shubhamagiwal92;24018048",
+  _FS = _prefix++_Gs,
+  createHash(_FS).
 
 createHash(_A)->
   _BinaryCypher = crypto:hash(sha256,_A),
   _IntegerCypher = binary:decode_unsigned(_BinaryCypher),
   _Hash = integer_to_list(_IntegerCypher,16),
-  _Hash.
+  %%io:fwrite(" Hash length is  ~w \n", [string:length(_Hash)]),
+  _Length = string:length(_Hash),
+  if
+    _Length ==60 -> io:fwrite("String: ~p hash: ~p \n",[_A,"0000"++_Hash]);
+    true -> main(1)
+  end.
+
+createActors()->
+  F = fun(X)-> timer: sleep(10), main(1) end,
+  [spawn(fun()->F(X)end)||X<-lists:seq(1,10)].
+
+
+
+
+
+
+
+
+%%  _zerocount  = string:sub_string(_Hash,1,1),
+%%  checkCoin(_zerocount).
+%%checkCoin(_zeros) when _zeros /= "0"->
+%%  main(2).
+
+
 %%io_lib:format("String is ~64.16.0b",[_IntegerCypher]).
 
